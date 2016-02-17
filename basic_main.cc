526803304b5ae533e975d1cdf673f7761386cc20
yuncliu <lyc1990@qq.com>
2016-02-17 11:24:55 +0800

    add variable declare

--- basic_main.cc
+++ basic_main.cc
@@ -13,24 +13,30 @@ static IRBuilder<> Builder(getGlobalContext());
 static std::map<std::string, Value *> NamedValues;
 int main(int argc, char const* argv[])
 {
-	TheModule = llvm::make_unique<Module>("my cool jit", getGlobalContext());
-	// Make the function type:  int (int,char**) etc.
-	std::vector<Type*> args;
+    TheModule = llvm::make_unique<Module>("main", getGlobalContext());
+    // Make the function type:  int (int,char**) etc.
+    std::vector<Type*> args;
     args.push_back(Type::getInt32Ty(getGlobalContext()));
     PointerType* x = Type::getInt8PtrTy(getGlobalContext());
     PointerType* p = PointerType::get(x, 0);
     args.push_back(p);
-	FunctionType *FT = FunctionType::get(Type::getInt32Ty(getGlobalContext()), args, false);
+    FunctionType *FT = FunctionType::get(Type::getInt32Ty(getGlobalContext()), args, false);
 
-	//Function *F = Function::Create(FT, GlobalValue::InternalLinkage, "main", TheModule.get());
-	Function *F = Function::Create(FT, Function::ExternalLinkage, "main", TheModule.get());
-    for (auto& arg : F->args()) {
-        arg.setName("argc");
-    }
+    //Function *F = Function::Create(FT, GlobalValue::InternalLinkage, "main", TheModule.get());
+    Function *F = Function::Create(FT, Function::ExternalLinkage, "main", TheModule.get());
+    Function::arg_iterator argit = F->arg_begin();
+    Value *arg_0 = argit++;
+    arg_0->setName("argc");
+    Value *arg_1 = argit++;
+    arg_1->setName("argv");
+
     BasicBlock *BB = BasicBlock::Create(getGlobalContext(), "entry", F);
     Builder.SetInsertPoint(BB);
+    AllocaInst *alloc = new AllocaInst(Type::getInt32Ty(getGlobalContext()), "Im_a_var", BB);
+    Value *xxx = ConstantInt::get(Type::getInt32Ty(getGlobalContext()), 1000, true);
+    Builder.CreateStore(xxx, alloc);
     Value *RetVal = ConstantInt::get(Type::getInt32Ty(getGlobalContext()), 0, true);
     Builder.CreateRet(RetVal);
-	TheModule->dump();
-	return 0;
+    TheModule->dump();
+    return 0;
 }
